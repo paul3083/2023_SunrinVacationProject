68aa47944e5b9a59d2ccc23dd14d7f885835f656
Doyun Yu <doyun0109@icloud.com>
2023-08-08 22:37:18 +0900

    edit props add Wrapper, Gap

--- src/components/Atomic.tsx
+++ src/components/Atomic.tsx
@@ -1,26 +1,55 @@
 import styled from 'styled-components/native';
-export const Column = styled.View`
-  display: flex;
-  flex-direction: column;
-`;
 
-export const Row = styled.View`
-  display: flex;
-  flex-direction: row;
-`;
-
-interface IFontTextProps {
-  fontSize?: number;
-  fontWeight?: number;
+interface ContainerProps {
+  gap?: number;
 }
 
-export const SuitText = styled.Text<IFontTextProps>`
+export const Column = styled.View<ContainerProps>`
+  display: flex;
+  flex-direction: column;
+  gap: ${props => props.gap || 0}px;
+`;
+
+export const Row = styled.View<ContainerProps>`
+  display: flex;
+  flex-direction: row;
+  gap: ${props => props.gap || 0}px;
+`;
+
+interface FontTextProps {
+  fontSize?: number;
+  fontWeight?: number;
+  color?: string;
+}
+
+export const SuitText = styled.Text<FontTextProps>`
   font-family: 'SUIT Variable';
   font-weight: ${props => props.fontWeight || 400};
   font-size: ${props => props.fontSize || 16}px;
+  color: ${props => props.color || '#1C1B1F'};
 `;
 
 export const StyledSafeAreaView = styled.SafeAreaView`
   flex: 1;
   background-color: #f5f6fa;
+`;
+
+interface WrapperProps {
+  padding?: number;
+}
+export const Wrapper = styled.View<WrapperProps>`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: ${props => props.padding || 0}px;
+`;
+
+interface GapProps {
+  width?: number;
+  height?: number;
+}
+
+export const Gap = styled.View<GapProps>`
+  width: ${props => props.width || 0}px;
+  height: ${props => props.height || 0}px;
 `;
