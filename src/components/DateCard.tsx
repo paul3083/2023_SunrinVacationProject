eea6879020b974d5585368a92caef27c15b71e83
Doyun Yu <doyun0109@icloud.com>
2023-08-11 06:54:02 +0900

    홈화면 달력

---
+++ src/components/DateCard.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {SuitText} from '@components/Atomic';
+import styled from 'styled-components/native';
+import {useSetRecoilState} from 'recoil';
+import SelectedDateIndexAtom from '@/atoms/SelectedDateIndexAtom';
+
+interface DateCardProps {
+  index: number;
+  selected: boolean;
+  date: number;
+  dayOfWeek: string;
+}
+
+const DateCard = ({index, selected, date, dayOfWeek}: DateCardProps) => {
+  const setSelectedDate = useSetRecoilState(SelectedDateIndexAtom);
+  const onPress = () => {
+    setSelectedDate(index);
+  };
+  return (
+    <Container
+      onPress={onPress}
+      style={
+        selected ? {backgroundColor: 'black'} : {backgroundColor: 'white'}
+      }>
+      <SuitText
+        weight={selected ? 700 : 500}
+        size={16}
+        color={selected ? 'white' : 'black'}>
+        {date}
+      </SuitText>
+      <SuitText
+        weight={selected ? 700 : 500}
+        size={16}
+        color={selected ? 'white' : 'black'}>
+        {dayOfWeek}
+      </SuitText>
+    </Container>
+  );
+};
+
+const Container = styled.TouchableOpacity`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+  width: 50px;
+  height: 80px;
+  border-radius: 12px;
+`;
+
+export default DateCard;
---
+++ src/components/DateCardList.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Row} from '@components/Atomic';
+import DateCard from '@components/DateCard';
+import selectedDateIndexAtom from '@/atoms/SelectedDateIndexAtom';
+import {useRecoilValue} from 'recoil';
+
+const DateCardList = () => {
+  const selectedDateIndex = useRecoilValue(selectedDateIndexAtom);
+  return (
+    <Row gap={23}>
+      {Array(5)
+        .fill(0)
+        .map((_, index) => {
+          const date = calculateDateAndDay(index).date;
+          const dayOfWeek = calculateDateAndDay(index).dayOfWeek;
+          return (
+            <DateCard
+              key={index}
+              index={index}
+              selected={index === selectedDateIndex}
+              date={date}
+              dayOfWeek={dayOfWeek}
+            />
+          );
+        })}
+    </Row>
+  );
+};
+
+const calculateDateAndDay = (offset: number) => {
+  const currentDate = new Date();
+  currentDate.setDate(currentDate.getDate() + offset);
+  const daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+  const dayIndex = currentDate.getDay();
+  const dayOfWeek = daysOfWeek[dayIndex];
+  const day = currentDate.getDate();
+  return {
+    date: day,
+    dayOfWeek: dayOfWeek,
+  };
+};
+
+export default DateCardList;
