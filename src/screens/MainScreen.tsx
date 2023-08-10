a4bcc514fb0c169940e4607e3baddb950cbbcc0f
Doyun Yu <doyun0109@icloud.com>
2023-08-11 08:40:30 +0900

    MainScreen UI 구현

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,43 +1,31 @@
 import React from 'react';
 import Header from '@components/Header';
-import {StyledSafeAreaView} from '@components/Atomic';
-import {Text, TouchableOpacity} from 'react-native';
-import {logOut} from '@/lib/auth';
+import {Gap, StyledSafeAreaView, SuitText, Wrapper} from '@components/Atomic';
+import DateCardList from '@components/DateCardList';
+import ScheduleCard from '@components/ScheduleCard';
+import LocationCardList from '@components/LocationCardList';
 
 const MainScreen = () => {
   return (
     <StyledSafeAreaView>
       <Header title={'홈'} />
-      <TouchableOpacity onPress={logOut}>
-        <Text>돌아가기</Text>
-      </TouchableOpacity>
+      <Wrapper padding={24}>
+        <SuitText weight={700} size={28} color={'#1C1B1F'}>
+          오늘 운동일정이 {'있습니다.'}
+        </SuitText>
+        <Gap height={16} />
+        <DateCardList />
+        <Gap height={24} />
+        <ScheduleCard title={'df'} time={new Date()} />
+        <Gap height={24} />
+        <SuitText weight={500} size={16} color={'#7B7B7D'}>
+          추천수가 가장 많은 장소
+        </SuitText>
+        <Gap height={8} />
+      </Wrapper>
+      <LocationCardList orderByHeartCount={true} />
     </StyledSafeAreaView>
   );
 };
-
-// const M = () => {
-//   return (
-//     <Row
-//       style={{
-//         width: '100%',
-//         margin: 24,
-//         justifyContent: 'space-between',
-//         backgroundColor: '#ffffff',
-//       }}>
-//       {/*<Day />*/}
-//       {/*<Day />*/}
-//       {/*<Day />*/}
-//       {/*<Day />*/}
-//       {/*<Day />*/}
-//     </Row>
-//   );
-// };
-//
-// const Day = styled.View`
-//   width: 50px;
-//   height: 80px;
-//   margin: 0 24px;
-//   background-color: #00bf00;
-// `;
 
 export default MainScreen;
