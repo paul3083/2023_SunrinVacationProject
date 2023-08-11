8ab532e4643a8c6c683a8e0b4040225fac645df2
Doyun Yu <doyun0109@icloud.com>
2023-08-12 01:14:32 +0900

    linking @atoms

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,7 @@ module.exports = {
           '@components': './src/components',
           '@screens': './src/screens',
           '@navigation': './src/navigation',
+          '@atoms': './src/atoms',
         },
       },
     ],
